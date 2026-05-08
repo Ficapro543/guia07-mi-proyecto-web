cb09a63df458ef2ccdae684535272b4c1a71b722
Ficapro543 <ficapro543@gmail.com>
2026-05-07 21:22:21 -0500

    Versión 3: Se mejora la interacción del sistema con JavaScript (se añadieron buenas prácticas al código para legibilidad)

--- script.js
+++ script.js
@@ -1,20 +1,39 @@
 function saludar() {
+
     // Obtener el valor del input
     let nombre = document.getElementById("nombre").value;
+
     // Validar si el usuario escribió algo
     if (nombre === "") {
+
         document.getElementById("resultado").innerText =
         "Por favor, ingresa tu nombre.";
+
     } else {
-    document.getElementById("resultado").innerText =
+
+        document.getElementById("resultado").innerText =
         "Hola " + nombre + ", bienvenido al sistema.";
+
+        // Limpiar input después del saludo
+        document.getElementById("nombre").value = "";
     }
 }
+
 function validarCorreo() {
+
     let correo = document.getElementById("correo").value;
+
     if (correo === "") {
-        document.getElementById("mensajeCorreo").innerText = "Debe ingresar un correo.";
+
+        document.getElementById("mensajeCorreo").innerText =
+        "Debe ingresar un correo.";
+
     } else {
-        document.getElementById("mensajeCorreo").innerText = "Correo registrado correctamente.";
+
+        document.getElementById("mensajeCorreo").innerText =
+        "Correo registrado correctamente.";
+
+        // Limpiar input después de validar
+        document.getElementById("correo").value = "";
     }
 }
